2114afd4557ab25d75eb8b1b0428541ac8cf1211
oriel <orielmalihi1@gmail.com>
2020-03-24 23:23:30 +0200

    all is working!

--- Test.cpp
+++ Test.cpp
@@ -85,6 +85,103 @@ TEST_CASE("Test replacement of g and j and replacement of lower-case and upper-c
     /* Add more checks here */
 }
 
+TEST_CASE("Test replacement of s and z and replacement of lower-case and upper-case") {
+    string text = "Zombie movies are the best";
+    CHECK(find(text, "zombie") == string("Zombie"));
+    CHECK(find(text, "sombie") == string("Zombie"));
+    CHECK(find(text, "SOMBie") == string("Zombie"));
+    CHECK(find(text, "bezt") == string("best"));
+    CHECK(find(text, "BeZt") == string("best"));
+    CHECK(find(text, "BEZT") == string("best"));
+    CHECK(find(text, "bEZt") == string("best"));
+    /* Add more checks here */
+}
+
+TEST_CASE("Test replacement of d and t and replacement of lower-case and upper-case") {
+    string text = "london bridge is falling down";
+    CHECK(find(text, "Down") == string("down"));
+    CHECK(find(text, "town") == string("down"));
+    CHECK(find(text, "Town") == string("down"));
+    CHECK(find(text, "DOWN") == string("down"));
+    CHECK(find(text, "towN") == string("down"));
+    CHECK(find(text, "ToWn") == string("down"));
+    CHECK(find(text, "dOWn") == string("down"));
+    /* Add more checks here */
+}
+
+TEST_CASE("Test replacement of o and u and replacement of lower-case and upper-case") {
+    string text = "london bridge is falling down";
+    CHECK(find(text, "Duwn") == string("down"));
+    CHECK(find(text, "duwn") == string("down"));
+    CHECK(find(text, "DuWn") == string("down"));
+    CHECK(find(text, "duWN") == string("down"));
+    CHECK(find(text, "dUwN") == string("down"));
+    CHECK(find(text, "dUWn") == string("down"));
+    CHECK(find(text, "DUWN") == string("down"));
+    /* Add more checks here */
+}
+
+TEST_CASE("Test replacement of i and y and replacement of lower-case and upper-case") {
+    string text = "london bridge is falling down";
+    CHECK(find(text, "brydge") == string("bridge"));
+    CHECK(find(text, "brIdge") == string("bridge"));
+    CHECK(find(text, "brYdge") == string("bridge"));
+    CHECK(find(text, "BRYDGE") == string("bridge"));
+    CHECK(find(text, "fallyng") == string("falling"));
+    CHECK(find(text, "FallYng") == string("falling"));
+    CHECK(find(text, "fallYNG") == string("falling"));
+    /* Add more checks here */
+}
+
+
+TEST_CASE("Test replacement more than one letter in a word") {
+    string text = "london bridge is falling down";
+    CHECK(find(text, "frydge") == string("bridge"));
+    CHECK(find(text, "FRYdje") == string("bridge"));
+    CHECK(find(text, "Bridge") == string("bridge"));
+    CHECK(find(text, "fridje") == string("bridge"));
+    CHECK(find(text, "fridJE") == string("bridge"));
+    CHECK(find(text, "FrYdge") == string("bridge"));
+    CHECK(find(text, "FrIdge") == string("bridge"));
+    CHECK(find(text, "prYdje") == string("bridge"));
+    CHECK(find(text, "PRYDJE") == string("bridge"));
+    CHECK(find(text, "fallinj") == string("falling"));
+    CHECK(find(text, "FallYnG") == string("falling"));
+    CHECK(find(text, "fallynj") == string("falling"));
+    /* Add more checks here */
+}
+
+TEST_CASE("test if the program throws an exeption for words that are not in the text") {
+    string text = "london bridge is falling down";
+    CHECK_THROWS(find(text, "aridge"));
+    CHECK_THROWS(find(text, "bridg"));
+    CHECK_THROWS(find(text, "bridgee"));
+    CHECK_THROWS(find(text, "izz"));
+    CHECK_THROWS(find(text, "i"));
+    CHECK_THROWS(find(text, "fall"));
+    CHECK_THROWS(find(text, "fallimg"));
+    CHECK_THROWS(find(text, "bridgh"));
+    CHECK_THROWS(find(text, "dowe"));
+    CHECK_THROWS(find(text, "dow"));
+    CHECK_THROWS(find(text, "dwn"));
+    CHECK_THROWS(find(text, "downe"));
+    CHECK_THROWS(find(text, "own"));
+    CHECK_THROWS(find(text, "sondon"));
+    CHECK_THROWS(find(text, "tondon"));
+    CHECK_THROWS(find(text, "londom"));
+    CHECK_THROWS(find(text, "his"));
+    CHECK_THROWS(find(text, "bcidge"));
+    CHECK_THROWS(find(text, "down!"));
+    CHECK_THROWS(find(text, "fakking"));
+    CHECK_THROWS(find(text, "falking"));
+    CHECK_THROWS(find(text, "falking"));
+    CHECK_THROWS(find(text, "lozdon"));
+    /* Add more checks here */
+}
+
+
+
+
 
 
 
